b1f89a74ea974ba7788bd4111c5e6c84da8c65f5
Shajadul <shajadulrahe@gmail.com>
2022-11-17 21:01:10 +0600

    feat: Add events to form

--- src/Components/Form/FormDetails.js
+++ src/Components/Form/FormDetails.js
@@ -1,19 +1,42 @@
+import React, { useState } from "react";
 import "./FormDetails.css";
 const FormDetails = () => {
+  const [enteredText, setEnteredText] = useState("");
+  const titleHandler = (event) => {
+    setEnteredText(event.target.value);
+    console.log(enteredText);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const amountHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    console.log(enteredAmount);
+  };
+  const [enteredDate, setEnteredDate] = useState("");
+  const dateHandler = (event) => {
+    setEnteredDate(event.target.value);
+    console.log(enteredDate);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input onChange={titleHandler} type="text" />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="10" step="10" />
+          <input onChange={amountHandler} type="number" min="10" step="10" />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            onChange={dateHandler}
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+          />
         </div>
       </div>
       <div className="new-expense__actions ">
